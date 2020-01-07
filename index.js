3464c4bbc90f36642db45efd41738effa85b70d4
Joe <Joe@Joe-Air.local>
2020-01-07 15:22:10 +0200

    decoding payloads functionality...

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder');
 
 const server = http.createServer((req, res) => {
   // parse the url
@@ -9,13 +10,32 @@ const server = http.createServer((req, res) => {
   const path = parsedURL.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  // get the http method
-  const method = req.method;
+  // get the query string as an object
+  const queryStringObj = parsedURL.query;
 
-  res.end('whats happening\n');
+  // get the http method
+  const method = req.method.toLowerCase();
+
+  // get the headers as an object
+  const headers = req.headers;
+
+  // get the payload
+  const decoder = new StringDecoder('utf8');
+  let buffer = '';
+
+  req.on('data', chunk => {
+    buffer += decoder.write(chunk);
+  });
+
+  req.on('end', () => {
+    buffer += decorder.end();
+    res.end('whats happening\n');
+  });
 
   console.log(method); // http method
   console.log(trimmedPath); // trimmed url path
+  console.log(queryStringObj); // query string object
+  console.log(headers); // headers
 });
 
-server.listen(3000, console.log('listening on port 3000'));
+server.listen(3000, () => console.log('listening on port 3000'));
